18b64177e500010615d1a3d2098b8b9ae3a7c9d4
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-02-15 18:33:54 +0200

    change theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,6 @@
 import { Roboto } from '@next/font/google';
 import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import { green, indigo, red } from '@mui/material/colors';
 
 export const roboto = Roboto({
   weight: ['300', '400', '500', '700'],
@@ -13,10 +13,10 @@ export const roboto = Roboto({
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: green[400],
     },
     secondary: {
-      main: '#19857b',
+      main: indigo[500],
     },
     error: {
       main: red.A400,
